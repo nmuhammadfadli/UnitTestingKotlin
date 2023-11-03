0fdde1e631eecf7cb2fd82000f00465d62af3ad6
nmuhammadfadli <fadligans98@gmail.com>
2023-11-03 15:39:13 +0700

    third commit

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,6 +1,14 @@
 
+//Saya membuat kelas dengan nama Calculator dengan bahasa Kotlin
+// Kelas ini akan berisi metode-metode dan properti-properti yang dapat digunakan untuk melakukan operasi perhitungan
 class Calculator {
+    // Disini saya membuat deklarasi fungsi/metode dalam kelas Calculator,bernama "Add" dan menerima dua parameter
     fun add(first: Int, second: Int): Int {
+    //first, bertipe data Integer, sebagai bilangan pertama yang akan dilakukan operasi penjumlahan
+    //second juga bertipe data Integer, sebagai bilangan kedua yang akan dilakukan operasi penjumlahan
+    //method ini juga memberikan deklarasi bahwa akan mengembalikan nilai dalam bentuk Integer setelah dieksekusi
         return first + second
+    // Disini, method ini mengambil dua parameter, yaitu first dan second, dan menjumlahkannya dengan operator penambahan +
+    //Hasil penjumlahan ini kemudian dikembalikan sebagai nilai balik (return) dari metode "add"
     }
 }
--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -1,12 +1,20 @@
+//Disini saya mengimpor anotasi @Test dari paket "org.junit.jupiter.api.Test"
+//Anotasi @test digunakan untuk memindai method yang akan kita uji
 import org.junit.jupiter.api.Test
 
+//Saya membuat kelas CalculatorTest yang akan digunakan sebagai pengujian kelas Calculator
+//Penamaan "Test" dibelakang nama, sebenarnya opsional, hanya untuk mempermudah kita dalam mengingat suatu kelas yang akan diuji
 class CalculatorTest {
+//Baris ini berguna untuk mendeklarasikan dan menginisialisasi variabel calculator dengan sebuah instansi objek dari kelas Calculator
+//Ini akan digunakan dalam metode pengujian untuk memanggil metode add dari kelas Calculator
     val calculator = Calculator()
-
-    @Test
-    fun testAddSuccess(){
+//Baris ini mendefinisikan metode pengujian dengan nama testAddSuccess
+    @Test //Anotasi @Test menandai metode ini sebagai metode pengujian
+    fun testAddSuccess(){//Baris ini mendefinisikan metode pengujian dengan nama testAddSuccess
         val result = calculator.add(10,10)
+    //Disini kita memanggil metode add dari objek calculator dengan parameter 10 dan 10, dan hasilnya disimpan dalam variabel result
     }
+    //Disini saya membuat pengujian yang sama dengan yang sevelumnya, dengan nama "testAddSuccess2"
     @Test
     fun testAddSuccess2(){
         val result = calculator.add(10,10)
